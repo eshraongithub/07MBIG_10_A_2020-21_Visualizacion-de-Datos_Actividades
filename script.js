04f6cd37cd6b0383ab0d383f7dd8d07d4c0f776a
eshraongithub <eshra14@hotmail.com>
2021-04-03 14:08:52 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
 //Gráfico de columnas. Mi 1º gráfico
 var visualization = d3plus.viz()
 .container("#viz1")
